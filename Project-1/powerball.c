028ab9e1748210fa3d2058696548fb9cef679a56
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-02-17 01:02:54 -0800

    add argument reading

--- Project-1/powerball.c
+++ Project-1/powerball.c
@@ -3,9 +3,39 @@
 int main(int argc, char *argv[]){
     printf("argc = %d\n", argc);
     printf("argv[0] - %s\n", argv[0]);
-    if (argc < 4) {
-        fprintf(stderr, "Argument count should be 4 or larger\n");
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s -n NumbersToGenerate -r MaxNumber [-p MaxPowerBallNumber] -N NumberSetsToGenerate\n", argv[0]);
         return 1;
+    }
+
+    int num_count = -1;
+    int max_num = -1;
+    int max_powerball =0;
+    int num_sets = -1;
+
+    for (int i = 0; i < argc; i++){
+        if (argv[i][0] == '-'){
+            switch (argv[i][1])
+            {
+            case 'n':
+                printf("case 'n'\n");
+                break;
+            case 'r':
+                printf("case 'r'\n");
+                break;
+            case 'p':
+                printf("case 'p'\n");
+                break;
+            case 'N':
+                printf("case N\n");
+                break;
+            
+            
+            default:
+                printf("case default\n");
+                break;
+            }
+        }
     }
 
     return 0;
