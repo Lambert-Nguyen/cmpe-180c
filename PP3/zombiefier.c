4902f21197fac9cdd774933c7cfbb4fa7fbaa939
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-09 00:16:08 -0800

    PP3: add taking command-line arguments

--- PP3/zombiefier.c
+++ PP3/zombiefier.c
@@ -6,3 +6,28 @@ Your program must be 100% reliable and create the zombies requested on every run
 Programs that are copies of another will all get zero grade. THIS IS AN INDIVIDUAL ASSIGNMENT as any future homework you may get. 
 REMINDER: late homework == 0 grade. Online submissions only. Programs that do not compile will get zero credit, programs that would not run will get zero credit.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sigcont_handler(int signo) {
+    continue_execution = 1;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3 || strcmp(argv[1], "-n") != 0) {
+        fprintf(stderr, "Usage: %s -n <num_zombies>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    
+    int n = atoi(argv[2]);
+    if (n <= 0) {
+        fprintf(stderr, "Please provide a positive integer for the number of zombies.\n");
+        return EXIT_FAILURE;
+    }
+
+}
