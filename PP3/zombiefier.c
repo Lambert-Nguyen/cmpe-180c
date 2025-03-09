fca22ac8417135d664ffa6c784b4ee844f03b6bd
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-08 23:43:07 -0800

    update requirements

---
+++ PP3/zombiefier.c
@@ -0,0 +1,8 @@
+/*Write a “C” program that creates a number of zombies (1 through n) processes as specified in the command line arguments to the program: zombiefier –n 10 creates 10 zombies. 
+Make sure the program cleans up the zombies upon completion. 
+Zombies must remain in the system until a signal (SIGCONT) is sent to zombifier. 
+BEWARE of solutions posted on the web that may not fulfill these requirements, if you copy such version you most likely will get zero points. 
+Your program must be 100% reliable and create the zombies requested on every run, if it does not, you will receive no points.
+Programs that are copies of another will all get zero grade. THIS IS AN INDIVIDUAL ASSIGNMENT as any future homework you may get. 
+REMINDER: late homework == 0 grade. Online submissions only. Programs that do not compile will get zero credit, programs that would not run will get zero credit.
+*/
