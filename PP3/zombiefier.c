19a3fc5651e1d11472f00e10f560f628e41f9a62
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-09 22:42:03 -0700

    Add zombifier logics

--- PP3/zombiefier.c
+++ PP3/zombiefier.c
@@ -6,13 +6,16 @@ Your program must be 100% reliable and create the zombies requested on every run
 Programs that are copies of another will all get zero grade. THIS IS AN INDIVIDUAL ASSIGNMENT as any future homework you may get. 
 REMINDER: late homework == 0 grade. Online submissions only. Programs that do not compile will get zero credit, programs that would not run will get zero credit.
 */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+
+volatile sig_atomic_t continue_execution = 0;
 
 void sigcont_handler(int signo) {
     continue_execution = 1;
@@ -21,13 +24,42 @@ void sigcont_handler(int signo) {
 int main(int argc, char *argv[]) {
     if (argc != 3 || strcmp(argv[1], "-n") != 0) {
         fprintf(stderr, "Usage: %s -n <num_zombies>\n", argv[0]);
-        return EXIT_FAILURE;
+        return 1;
     }
     
     int n = atoi(argv[2]);
-    if (n <= 0) {
-        fprintf(stderr, "Please provide a positive integer for the number of zombies.\n");
-        return EXIT_FAILURE;
+    if (n <= 0){
+        fprintf(stderr, "ERROR: Please provide a positive integer for the number of zombies.\n");
+        return 1;
+    }
+    pid_t zombie_pids[n]; 
+
+    signal(SIGCONT, sigcont_handler);
+
+    for (int i = 0; i < n; i++){
+        pid_t pid = fork();
+        if (pid<0){
+            fprintf(stderr, "Error: fork() failed - %s\n", strerror(errno));
+            return 1;
+        }
+        if (pid == 0){
+            exit(0);  // Child process exits immediately, becoming a zombie
+        } else {
+            zombie_pids[i] = pid;
+        }
+    }
+    
+    fprintf(stdout,"%d zombie processes created. Send SIGCONT to PID %d to clean up.\n", n, getpid());
+    
+    while(!continue_execution){
+        pause(); // Wait for SIGCONT
     }
 
+    fprintf(stdout,"Received SIGCONT, cleaning up zombies...\n");
+    for(int i = 0; i<n; i++){
+        waitpid(zombie_pids[i], NULL, 0);
+    }
+    
+    fprintf(stdout,"All zombies cleaned up. Exiting.\n");
+    return 0;
 }
