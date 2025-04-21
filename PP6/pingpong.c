c59ed0f8cee132ac3f029bebebd1dacc61455d6f
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-04-20 19:47:07 -0700

    PP6 update fprintf to stdout and stderr instead of printf

--- PP6/pingpong.c
+++ PP6/pingpong.c
@@ -5,21 +5,26 @@
 #include <signal.h>
 #include <time.h>
 #include <errno.h>
+#include <string.h>
 
 static volatile sig_atomic_t done = 0;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t  cond12 = PTHREAD_COND_INITIALIZER;
 static pthread_cond_t  cond21 = PTHREAD_COND_INITIALIZER;
-static int turn = 0;  // 1 => thread1’s turn, 2 => thread2’s turn
+static int turn = 1;  // 1 -> thread1’s turn, 2 -> thread2’s turn
 
 static void sigint_handler(int sig) {
     done = 1;
 }
 
 static void *thread1_func(void *arg) {
-    pthread_mutex_lock(&mutex);
-    // initial ping
-    printf("thread 1: ping thread 2\n");
+    if (pthread_mutex_lock(&mutex) != 0) {
+        fprintf(stderr, "thread1: pthread_mutex_lock: %s\n", strerror(errno));
+        return NULL;
+    }
+
+    /* initial ping */
+    fprintf(stdout, "thread 1: ping thread 2\n");
     fflush(stdout);
     turn = 2;
     pthread_cond_signal(&cond12);
@@ -28,15 +33,16 @@ static void *thread1_func(void *arg) {
         struct timespec ts;
         clock_gettime(CLOCK_REALTIME, &ts);
         ts.tv_sec += 1;
-        // wait until it's our turn or timeout
+
         while (turn != 1 && !done) {
             if (pthread_cond_timedwait(&cond21, &mutex, &ts) == ETIMEDOUT)
                 break;
         }
         if (done || turn != 1) break;
-        printf("thread 1: pong! thread 2 ping received\n");
+
+        fprintf(stdout, "thread 1: pong! thread 2 ping received\n");
         fflush(stdout);
-        printf("thread 1: ping thread 2\n");
+        fprintf(stdout, "thread 1: ping thread 2\n");
         fflush(stdout);
         turn = 2;
         pthread_cond_signal(&cond12);
@@ -47,21 +53,25 @@ static void *thread1_func(void *arg) {
 }
 
 static void *thread2_func(void *arg) {
-    pthread_mutex_lock(&mutex);
+    if (pthread_mutex_lock(&mutex) != 0) {
+        fprintf(stderr, "thread2: pthread_mutex_lock: %s\n", strerror(errno));
+        return NULL;
+    }
 
     while (!done) {
         struct timespec ts;
         clock_gettime(CLOCK_REALTIME, &ts);
         ts.tv_sec += 1;
-        // wait until it's our turn or timeout
+
         while (turn != 2 && !done) {
             if (pthread_cond_timedwait(&cond12, &mutex, &ts) == ETIMEDOUT)
                 break;
         }
         if (done || turn != 2) break;
-        printf("thread 2: pong! thread 1 ping received\n");
+
+        fprintf(stdout, "thread 2: pong! thread 1 ping received\n");
         fflush(stdout);
-        printf("thread 2: ping thread 1\n");
+        fprintf(stdout, "thread 2: ping thread 1\n");
         fflush(stdout);
         turn = 1;
         pthread_cond_signal(&cond21);
@@ -71,23 +81,33 @@ static void *thread2_func(void *arg) {
     return NULL;
 }
 
-int main(void) {
-    struct sigaction sa = { .sa_handler = sigint_handler };
+int main(void){
+    struct sigaction sa={ .sa_handler = sigint_handler };
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        fprintf(stderr, "sigaction: %s\n", strerror(errno));
+        exit(1);
+    }
 
     pthread_t t1, t2;
     if (pthread_create(&t1, NULL, thread1_func, NULL) != 0) {
-        perror("pthread_create");
+        fprintf(stderr, "pthread_create(thread1): %s\n", strerror(errno));
         exit(1);
     }
     if (pthread_create(&t2, NULL, thread2_func, NULL) != 0) {
-        perror("pthread_create");
+        fprintf(stderr, "pthread_create(thread2): %s\n", strerror(errno));
         exit(1);
     }
 
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    if (pthread_join(t1, NULL) != 0) {
+        fprintf(stderr, "pthread_join(thread1): %s\n", strerror(errno));
+        exit(1);
+    }
+    if (pthread_join(t2, NULL) != 0) {
+        fprintf(stderr, "pthread_join(thread2): %s\n", strerror(errno));
+        exit(1);
+    }
+
     return 0;
 }
